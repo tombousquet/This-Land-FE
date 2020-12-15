ba82d8fa0d1f478279ae048c7a35958450158da0
Tom Bousquet <tom.bousquet@alumni.unc.edu>
2020-12-14 21:23:27 -0500

    draggable marker on map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import './App.css'
 
@@ -11,9 +11,21 @@ function App () {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/tombousquet/ckinqejtv0v2617ms4kflvkp8',
+      // centered on durham
       center: [-78.8986, 35.9940],
       zoom: 13
     })
+    // floating marker
+    const marker = new mapboxgl.Marker({
+      color: '#999999',
+      anchor: 'bottom',
+      draggable: true
+    }).setLngLat([-78.9035, 35.992])
+      .addTo(map)
+    // track longitude and latitude
+    const lngLat = marker.getLngLat()
+    console.log('Long: ' + lngLat.lng + ', Lat: ' + lngLat.lat)
+    // zoom buttons
     map.addControl(new mapboxgl.NavigationControl(), 'top-right')
     return () => map.remove()
   }, [])
